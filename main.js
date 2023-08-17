af791cbd0c69035f8f364d8d9b0b578310b2b202
Jurabek0418 <jurabeknematov0418@gmail.com>
2023-08-17 10:23:02 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,90 @@
+var elForm = document.querySelector(".js-form");
+var elInput = elForm.querySelector(".js-input");
+var elMoviesList = document.querySelector(".js-movies-list");
+var elMovieSelect = document.querySelector("#js-select");
+function moviesCardCreating(arr){
+
+  elMoviesList.innerHTML = "";
+
+  for (const movie of arr) {
+
+
+    var elMovieItem = document.createElement("li");
+    elMovieItem.classList.add("movie-item");
+    var elMovieImg = document.createElement("img"); 
+    elMovieImg.classList.add("movie-img");
+    var elMovieTitle = document.createElement("h3"); 
+    elMovieTitle.classList.add("movie-title");
+    var elMovieImdbRating = document.createElement("span");
+    elMovieImdbRating.classList.add("imdb");
+    var elMovieYear = document.createElement("time"); 
+    elMovieYear.classList.add("year");
+    var elMovieRuntime = document.createElement("time");
+    elMovieRuntime.classList.add("text-white");
+    var elMovieCategory = document.createElement("p"); 
+    elMovieCategory.classList.add("genre");
+    var elMovieLink = document.createElement("a");
+    elMovieLink.classList.add("more-info");
+    
+    elMovieImg.setAttribute("src", `https://i3.ytimg.com/vi/${movie.ytid}/maxresdefault.jpg`);
+    elMovieTitle.textContent = movie.Title;
+    elMovieImdbRating.textContent = movie.imdb_rating;
+    elMovieYear.textContent = movie.movie_year;
+    elMovieRuntime.textContent = movie.runtime;
+    elMovieCategory.textContent = movie.Categories;
+    elMovieLink.textContent = "More info...";
+    
+    function getTime(time) {
+      
+      var hour = Math.floor(time / 60)
+      var minut = Math.floor(time % 60)
+      
+      return `${hour} hrs ${minut} min`
+    };
+    elMovieItem.append(elMovieImg, elMovieTitle, elMovieImdbRating, elMovieYear, getTime(movie.runtime), elMovieCategory, elMovieLink);
+    elMoviesList.appendChild(elMovieItem);
+  }
+}
+
+function selectFunc(){
+  var newArr = [];
+  movies.forEach(item => {
+  item.Categories.split("|").forEach(function(getGenre){
+    if(!newArr.includes(getGenre)){
+      newArr.push(getGenre)
+    }
+  })
+});
+
+newArr.forEach(gen => {
+  var elSelectOption = document.createElement("option");
+  elSelectOption.textContent = gen;
+  elSelectOption.value = gen;
+  elMovieSelect.appendChild(elSelectOption);
+
+});
+}
+
+elForm.addEventListener("submit", function (evt) {
+  
+  evt.preventDefault();
+  
+  var searchValue = elInput.value.trim();
+  var selectValue = elMovieSelect.value
+  var newRegexp = new RegExp(searchValue, "gi");
+  
+  var result = movies.filter(item => {
+
+    return item.Title.toString().match(newRegexp) && (item.Categories.includes(selectValue) || selectValue ==("All") 
+    );
+  });
+  
+  if(result.length > 0){
+    moviesCardCreating(result);
+  } else {
+    elMoviesList.textContent = "Not Found !"
+  }
+  
+})
+moviesCardCreating(movies);
+selectFunc();
---
+++ math.js
@@ -0,0 +1,72 @@
+// ======= Convert minutes to sec ======== 
+
+// function minsToSec(min) {
+
+//   var sec = (min * 60);
+
+//   return sec;
+// };
+// console.log(minsToSec());
+
+// ======= zero to the end of array ==========
+
+// var numbers = [1, 3, 0, 5, 0, 7, 0];
+// var result = [];
+
+// function zeroEnd(){
+
+//   for (const num of numbers) {
+//     if(num == 0) {
+//       result.push(num);
+//     } else {
+//       result.unshift(num);
+//     }
+//   }
+//   return result;
+// }
+// console.log(zeroEnd(numbers).join(" "));
+
+// ======== Math.abs add Math.abs function ========
+
+// var numbers = [1, 2, 0, -1, -2, 3, 4]
+// var arr = 0;
+// function additionAbsNumbers(){
+//   for (const num of numbers) {
+//     if(num > 0) {
+//       arr = num + arr;
+//     } 
+//   }
+//   return arr;
+// }
+// console.log(additionAbsNumbers(numbers));
+
+// ======= Count Boolean(true) =======
+
+// var trueyType = [false, true, false, true, true];
+// var output = [];
+// function countTrue(){
+//   for (const item of trueyType) {
+//     if(item == true) {
+//       output.push(item);
+//     } 
+//   }
+//   return output;
+// }
+// console.log(countTrue(trueyType).length);
+
+
+// ======= Find Highest Price Name ========
+
+// var list = [{name: "Ali", price:200},{name: "Jo'rabek", price: 800},{name: "Sardor", price: 400}];
+// var highestPriceName = ""
+// var highestPrice = 0;
+// function findHighestPriceName(){
+//   for (const item of list) {
+//     if(item.price > highestPrice){
+//       highestPrice = item.price;
+//       highestPriceName = item.name;
+//     }
+//   }
+//   return highestPriceName;
+// }
+// console.log(findHighestPriceName(list));
